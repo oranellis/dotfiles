087c7517b753e05cfb9d4b9d5f9939c2064b68ee
Oran Ellis <oran.mcnair@gmail.com>
2025-01-29 11:20:26 +0000

    feat: Wrap at words for markdown files instead of characters

--- .config/nvim/lua/filetype_specific.lua
+++ .config/nvim/lua/filetype_specific.lua
@@ -17,6 +17,9 @@ vim.api.nvim_create_autocmd({"BufRead"}, {
     pattern = {'*.md'},
     callback = function(_)
         vim.opt_local.wrap = true
+        vim.opt_local.linebreak = true
+        vim.opt_local.breakindent = true
+        vim.opt_local.breakindentopt = 'shift:2'
     end
 })
 
