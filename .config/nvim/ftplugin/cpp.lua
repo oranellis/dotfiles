07733edef18e6461b3158d7de164e39f841af08a
Oran Ellis <oran.mcnair@gmail.com>
2025-11-18 17:12:36 +0000

    feat: Re-add clangd source/header switching for c/c++ files

--- .config/nvim/ftplugin/cpp.lua
+++ .config/nvim/ftplugin/cpp.lua
@@ -1,3 +1,10 @@
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function (ev)
+    local opts = { buffer = ev.buf, silent = true, noremap = true }
+    vim.keymap.set('n', '<leader>h', vim.cmd.LspClangdSwitchSourceHeader, opts)
+  end
+})
+
 vim.api.nvim_create_autocmd("BufWritePre", {
   callback = function()
     vim.lsp.buf.format({async = false})
