6427015977dfeacb4fea25004a5a2d939c6f1371
Oran Ellis <mail@oranellis.com>
2026-09-15 10:35:05 +0100

    feat: make git update work without remote

--- .config/git-update.sh
+++ .config/git-update.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
 if [ -z "$1" ]; then
-	commit_msg="Update $(date '+%y%m%d %H%M')"
+    commit_msg="Update $(date '+%y%m%d %H%M')"
 else
-	remaining_args="$*"
-	if [[ ${remaining_args:0:1} =~ [a-zA-Z] ]]; then
-		# Capitalize the first letter and concatenate the rest of the string
-		commit_msg="${remaining_args^}"
-	else
-		commit_msg="$remaining_args"
-	fi
+    remaining_args="$*"
+    if [[ ${remaining_args:0:1} =~ [a-zA-Z] ]]; then
+        # Capitalize the first letter and concatenate the rest of the string
+        commit_msg="${remaining_args^}"
+    else
+        commit_msg="$remaining_args"
+    fi
 fi
 
-git reset && git pull && git add -A && git commit -m "$commit_msg" && git push
+if [ -n "$(git remote)" ]; then
+    git reset
+    git pull
+fi
+
+git add -A && git commit -m "$commit_msg"
+
+if [ -n "$(git remote)" ]; then
+    git push
+fi
